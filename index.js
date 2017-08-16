2dafbb49aa4f5a0292d21fc1e0e3db4151326268
pigio <piergiorgio.marin@arcadia-consulting.it>
2017-08-16 15:39:54 +0200

    basic settings

--- index.js
+++ index.js
@@ -14,9 +14,40 @@ controller.spawn({
 	if (err) {
 		throw new Error ('Error connecting to Slack: ', err);
 	}
-	console.log('Connectedto Slack');
+	console.log('Connected to Slack');
 }
 
 );
 
-var witbot = Witbot(witToken);
+var wit_bot = Witbot(witToken);
+
+controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
+  var wit = wit_bot.process(message.text, bot, message)
+
+  wit.hears('saluti_iniziali', 0.53, function (bot, message, outcome) {
+    bot.startConversation(message, function (_, convo) {
+      convo.say('Hello!')
+      convo.ask('How are you?', function (response, convo) {
+        witbot.process(response.text)
+          .hears('good', 0.5, function (outcome) {
+            convo.say('I am so glad to hear it!')
+            convo.next()
+          })
+          .hears('bad', 0.5, function (outcome) {
+            convo.say('I\'m sorry, that is terrible')
+            convo.next()
+          })
+          .otherwise(function (outcome) {
+            convo.say('I\'m cofused')
+            convo.repeat()
+            convo.next()
+          })
+      })
+    })
+  })
+
+  wit.otherwise(function (bot, message) {
+    bot.reply(message, 'You are so intelligent, and I am so simple. I don\'t understnd')
+  })
+})
+
